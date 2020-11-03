6edecfad620c1141d622e7b290bcd2198a31ee47
Maxime Zegres <maxime.zegres@gmail.com>
2020-11-03 22:29:16 +0100

    Change environment settings

--- src/OrderAPI/Startup.cs
+++ src/OrderAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Npgsql;
 using OrderAPI.Data;
 
 namespace OrderAPI
@@ -26,8 +27,12 @@ namespace OrderAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<OrderApiContext>(opt => opt.UseNpgsql
-                            (Configuration.GetConnectionString("PostgreSqlConnection")));
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<OrderApiContext>(opt => opt.UseNpgsql(builder.ConnectionString));
 
             services.AddControllers();
 
