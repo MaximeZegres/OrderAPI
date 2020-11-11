aa1ce244aa13a953ecc03bb8098b677168d780fd
Maxime Zegres <maxime.zegres@gmail.com>
2020-11-11 09:53:55 +0100

    Setup Angular Project and Add Angular-Material UI

--- src/OrderManagement/src/app/app.module.ts
+++ src/OrderManagement/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrderListComponent } from './order/order-list/order-list.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
